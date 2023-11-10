8c2bf2a198dd712f203373c55dc60248fe590b0f
mrshabel <sgumah001@st.ug.edu.gh>
2023-11-10 13:05:52 +0000

    api fix

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -40,17 +40,23 @@ export default async function TicketDetails({ params }) {
 
   return (
     <main>
-      <nav>
-        <h2>Ticket Details</h2>
-      </nav>
-      <div className="card">
-        <h3>{ticket.title}</h3>
-        <small>Created by {ticket.user_email}</small>
-        <p>{ticket.body}</p>
-        <div className={`pill ${ticket.priority}`}>
-          {ticket.priority} priority
-        </div>
-      </div>
+      {!ticket || ticket.length === 0 ? (
+        <p>Sorry, no tickets available now</p>
+      ) : (
+        <>
+          <nav>
+            <h2>Ticket Details</h2>
+          </nav>
+          <div className="card">
+            <h3>{ticket.title}</h3>
+            <small>Created by {ticket.user_email}</small>
+            <p>{ticket.body}</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </div>{" "}
+        </>
+      )}
     </main>
   );
 }
